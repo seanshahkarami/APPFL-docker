9a394d2af877ef1299c27d1c56625554eb43dc6f
Sean Shahkarami <sean.shahkarami@gmail.com>
2024-08-30 13:08:12 -0500

    removed r32.7.1

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -4,5 +4,4 @@
 docker buildx build --push --platform linux/amd64,linux/arm64 -t seanshahkarami/appfl .
 
 # build jetson images
-docker build -f Dockerfile.jetson --build-arg JETSON_VERSION=r32.7.1 -t seanshahkarami/appfl:r32.7.1 .
 docker build -f Dockerfile.jetson --build-arg JETSON_VERSION=r35.3.1 -t seanshahkarami/appfl:r35.3.1 .
